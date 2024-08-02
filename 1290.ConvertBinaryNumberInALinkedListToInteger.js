77d8d080d808e2a81b34e1ed3ec45865471ccf47
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-08-02 13:55:06 -0700

    Convert binary num in linked list

---
+++ 1290.ConvertBinaryNumberInALinkedListToInteger.js
@@ -0,0 +1,12 @@
+var getDecimalValue = function (head) {
+  total = 0;
+  let current = head;
+  if (head == null) {
+    return 0;
+  }
+  while (current != null) {
+    total = total * 2 + current.val;
+    current = current.next;
+  }
+  return total;
+};
